ffdc529b23b9020d82392abdd11d4632f29fd5bf
Seruhio30 <sh0041314@gmail.com>
2025-07-27 22:37:33 -0600

    h

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -2,8 +2,6 @@ const timestampInput = document.getElementById("timestamp");
 const now = new Date().toISOString();
 timestampInput.value = now;
 
-
-
 document.querySelectorAll("[data-modal-target]").forEach(link => {
   link.addEventListener("click", e => {
     e.preventDefault();

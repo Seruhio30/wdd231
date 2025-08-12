04d68ce0f22403d11fe9e6dff424011bc2d6d7cc
Seruhio30 <sh0041314@gmail.com>
2025-08-11 22:57:58 -0600

    footer cambios

--- finalproject/scripts/navigation.js
+++ finalproject/scripts/navigation.js
@@ -1,12 +1,9 @@
 export function navigation() {
      const navButtom = document.querySelector('#hamburger');
-     console.log('navButtom:', navButtom);
-
-
      const navLinks = document.querySelector('#nav-bar');
-    console.log('navLinks:', navLinks);
+
      navButtom.addEventListener('click', () => {
-          console.log('Hamburger clicked');
+
           navButtom.classList.toggle('show')
           navLinks.classList.toggle('show')
      });
